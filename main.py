abbccc8d123223a9393ffd89d100d869eb2ba658
KingOneF <jefwilkins@gmail.com>
2025-11-13 09:16:25 -0600

    CHapter 2 done

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
+from stats import get_chars_dict, count_words
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
+    chars_dict = get_chars_dict(text)
     print(f"Found {num_words} total words")
-
+    print(chars_dict)
 
 def get_book_text(path):
     with open(path, encoding="utf-8") as f:
         return f.read()
 
-def count_words(text: str) -> int:
-   # Splits on any whitespace (spaces, newlines, tabs), which is perfect for word counting
-    return len(text.split())
-
 if __name__ == "__main__":
     main()
-
-
 
 
---
+++ stats.py
@@ -0,0 +1,13 @@
+def count_words(text: str) -> int:
+    words = text.split()
+    return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
